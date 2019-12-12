56169af210d95c24f424a3553aed0f34ec490955
Lithium-Plus <36727811+Lithium-Plus@users.noreply.github.com>
2019-12-13 01:46:46 +0800

    fix

--- convert_data.py
+++ convert_data.py
@@ -24,7 +24,12 @@ for feature in data['features']:
         solved = 0
         area = polygon.area
         monthly = [0 for i in range(12)]
-        crime_types = []
+        robbery = 0
+        burglary = 0
+        auto_theft = 0
+        agg_assault = 0
+        rape = 0
+        theft = 0
         days = []
         for index, row in df.iterrows():
             point = Point((float(row['Longitude']),float(row['Latitude'])))
@@ -35,19 +40,35 @@ for feature in data['features']:
                 month_num = strptime(month,'%b').tm_mon
                 monthly[month_num-1] += 1
                 row['neighborhood'] = name
-                crime_type = row['Highest NIBRS/UCR Offense Description']
-                crime_types.append(crime_type)
                 if row['Clearance Status'] == 'C':
                     solved += 1
                     report_date = row['GO Report Date'][:-2] + '20' + row['GO Report Date'][-2:]
                     clear_date = row['Clearance Date'][:-2] + '20' + row['Clearance Date'][-2:]
                     delta = datetime.strptime(clear_date, '%d-%b-%Y').date() - datetime.strptime(report_date, '%d-%b-%Y').date()
                     days.append(delta.days)
+                if row['Highest NIBRS/UCR Offense Description'] == 'Robbery':
+                    robbery+=1
+                elif row['Highest NIBRS/UCR Offense Description'] == 'Burglary':
+                    burglary +=1 
+                elif row['Highest NIBRS/UCR Offense Description'] == 'Auto Theft':
+                    auto_theft +=1
+                elif row['Highest NIBRS/UCR Offense Description'] == 'Agg Assault':
+                    agg_assault += 1
+                elif row['Highest NIBRS/UCR Offense Description'] == 'Theft':
+                    theft +=1
+                elif row['Highest NIBRS/UCR Offense Description'] == 'Rape':
+                    rape += 1
 
         feature['properties']['density'] = count/(1000*area)
         feature['properties']['monthly'] = monthly
         feature['properties']['clearance_rate'] = solved/count if count > 0 else 0
-        feature['properties']['crime_types'] = crime_types
+        feature['properties']['robbery'] = robbery
+        feature['properties']['burglary'] = burglary
+        feature['properties']['auto_theft'] = auto_theft
+        feature['properties']['agg_assault'] = agg_assault
+        feature['properties']['theft'] = theft
+        feature['properties']['agg_assault'] = agg_assault
+        
         if len(days) > 0:
             feature['properties']['avg_days_solve'] = sum(days)/len(days)
         else:
